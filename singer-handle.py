52acbdcc5dace3936b68c7a7d8e1b0807f5e1027
antlers <antlers@illucid.net>
2024-11-14 11:13:42 -0800

    singer-handle: Adjust curve, add pins
    * singer-handle.py: Adjust curve, add pins

--- singer-handle.py
+++ singer-handle.py
@@ -10,16 +10,18 @@ def acc(n):
 
 thickness = 1.85
 
-initialRadius = 12.72 / 2
-minRadius     = 9.1 / 2
-minOffset     = acc(12.15)
-maxRadius     = 19 / 2
-maxOffset     = acc(22)
-finalRadius   = 13.33 / 2
-finalOffset   = acc(12.75)
+initialRadius    = 12.72 / 2 - 0.05
+penInitialRadius = 12.72 / 2
+minRadius        = 9.1 / 2
+minOffset        = acc(12.15)
+maxRadius        = 19 / 2
+maxOffset        = acc(22)
+finalRadius      = 13.33 / 2
+finalOffset      = acc(12.75 + 1.75)
 
 outer_edge = Spline(
     (initialRadius, 0),
+    (penInitialRadius, 0.0125),
     (minRadius, minOffset),
     (maxRadius, maxOffset),
     (finalRadius, finalOffset),
@@ -34,23 +36,58 @@ s += Line((0, finalOffset), (0, 0))
 # Revolve & Hollow out
 s = Plane.XZ * make_face(s)
 s = revolve(s, Axis.Z)
-s -= offset(s, -thickness)
+# s -= offset(s, -thickness)
 
 # # Punch ends back out
 # s -= extrude(Circle(initialRadius + thickness), thickness)
 # s -= Pos(0,0,finalOffset) * extrude(Circle(finalRadius + thickness), -thickness)
 
+# Punch out stem
+stem = extrude(Circle(initialRadius - 2 * thickness), finalOffset)
+
 # Split in half
-top = split(s, Plane.XZ, keep=Keep.TOP)
-bottom = split(s, Plane.XZ, keep=Keep.BOTTOM)
+top = split(s, Plane.XZ, keep=Keep.TOP) - stem
+bottom = split(s, Plane.XZ, keep=Keep.BOTTOM) - stem
 
 l = Rot(90, 0, 0) * outer_edge
 l1 = Spline(l @ 0.6, l @ 0.7, l @ 0.8)
 l2 = Spline(l @ 0.2, l @ 0.3, l @ 0.4)
 
+# Pegs
+offset = 5.25
+line = Line([(offset,0,0),(offset,0,finalOffset)])
+
+pegs = [
+  # Point, Radius, Height
+  (               line @ 0.05,  1.5,  3.00),
+  (Pos(0.4,0,0) * line @ 0.65,  1.7,  3.75),
+  (Pos(1,0,0)   * line @ 0.775, 2,    4.75),
+  (Pos(0.5,0,0) * line @ 0.9,   1.7,  4.00)
+]
+
+diff = 0.1
+thin_points = [Location(p) * extrude(Circle(r-diff).rotate(Axis.X, -90), h-(2*diff)) for p,r,h in pegs]
+thin_points += [p.mirror(Plane.ZY) for p in thin_points]
+thin_points = [fillet(p.edges()[2], 0.5+diff) for p in thin_points]
+top += thin_points
+
+thick_points = [Location(p) * extrude(Circle(r).rotate(Axis.X, -90), h) for p,r,h in pegs]
+thick_points += [p.mirror(Plane.ZY) for p in thick_points]
+thick_points = [fillet(p.edges()[2], 0.5-diff) for p in thick_points]
+bottom -= thick_points
+
+boxes = Pos(0,-maxRadius - 2,maxOffset) * Box(15,5,30)
+boxes = [boxes, boxes.mirror(Plane.XZ)]
+top -= boxes
+bottom -= boxes
+
 reset_show()
+# show_object(s)
+# show_object(top)
+# show_object(boxes)
 show_object(top)
-# show_object(bottom)
-# export_step(top, "top.step")
-# export_step(bottom, "bottom.step")
+show_object(bottom)
+# show_object(points)
+export_step(top, "top.step")
+export_step(bottom, "bottom.step")
 # %%
